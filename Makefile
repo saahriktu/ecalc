a771873ece6d5b9879afd0d2c181d46a3ad63e0c
saahriktu <saahriktu@gmail.com>
2019-12-29 22:11:35 +0300

    v3.4

--- Makefile
+++ Makefile
@@ -1,4 +1,14 @@
+ifeq ($(prefix),)
+	prefix=/usr/local
+endif
+ifeq ($(shell which $(CC)),)
+	CC=gcc
+endif
 all:
-	gcc -o ecalc ecalc.c -lm
+	$(CC) $(CFLAGS) -o ecalc ecalc.c
+	strip -S ecalc
 install:
-	cp ecalc /usr/bin
+	if [ ! -d "$(DESTDIR)$(prefix)/bin" ]; then mkdir -p $(DESTDIR)$(prefix)/bin; fi
+	install -m755 ecalc $(DESTDIR)$(prefix)/bin
+clean:
+	rm ecalc
--- ecalc.c
+++ ecalc.c
@@ -1,9 +1,10 @@
 /*
- * Easy CALCulator v3.2
+ * Easy CALCulator v3.4
  * noninteractive RPN commandline calculator
  * License: GNU GPLv3
  * Author: Artem Kurashov
  */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -11,47 +12,49 @@
 int main()
 {
     double x = 0, y = 0;
-    char c;
-    while ((c = getchar()) != EOF) {
-	switch (c) {
-	case 'c':
-	    x = 0;
-	    y = 0;
-	    break;
-	case '+':
-	    y += x;
-	    break;
-	case '-':
-	    y -= x;
-	    break;
-	case '*':
-	    y *= x;
-	    break;
-	case '/':
-	    y /= x;
-	    break;
-	case '^':
-	    y = pow(y, x);
-	    break;
-	case 's':
-	    x = sin(x);
-	    break;
-	case 'o':
-	    x = cos(x);
-	    break;
-	case 't':
-	    x = tan(x);
-	    break;
-	case 'l':
-	    x = log(x);
-	    break;
-	case 'q':
-	    return 0;
-	case '=':
-	    printf("%f,%f\n", x, y);
-	    break;
-	default:
-	    (void) scanf("%lf", &x);
-	}
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+        switch (c)
+        {
+        case 'c':
+            x = 0;
+            y = 0;
+            break;
+        case '+':
+            y += x;
+            break;
+        case '-':
+            y -= x;
+            break;
+        case '*':
+            y *= x;
+            break;
+        case '/':
+            y /= x;
+            break;
+        case '^':
+            y = pow(y, x);
+            break;
+        case 's':
+            x = sin(x);
+            break;
+        case 'o':
+            x = cos(x);
+            break;
+        case 't':
+            x = tan(x);
+            break;
+        case 'l':
+            x = log(x);
+            break;
+        case 'q':
+            return 0;
+        case '=':
+            printf("%.15f,%.15f\n", x, y);
+            break;
+        default:
+            (void) scanf("%lf", &x);
+        }
     }
 }
